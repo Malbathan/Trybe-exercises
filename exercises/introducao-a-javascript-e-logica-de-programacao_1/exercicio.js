657fea49558d6022652333de1801a761c6a24eda
Malbathan Lima <malbathanlima@gmail.com>
2021-02-24 18:47:29 -0300

    programa para exibir o maior entre 3 variaveis

--- exercises/introducao-a-javascript-e-logica-de-programacao_1/exercicio.js
+++ exercises/introducao-a-javascript-e-logica-de-programacao_1/exercicio.js
@@ -7,8 +7,18 @@ let b = 5;
 // console.log (a / b);
 // console.log (a % b);
 
-if (a > b) {
-    console.log(a);
+// if (a > b) {
+//     console.log(a);
+// } else {
+//     console.log(b);
+// }
+
+let c = 7;
+
+if (a > b && a > c) {
+    console.log (a);
+} else if (b > a && b > c) { 
+    console.log (b);
 } else {
-    console.log(b);
+    console.log (c);
 }
