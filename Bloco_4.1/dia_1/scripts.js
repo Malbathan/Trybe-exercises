9fbebd0f3cf55be64e9f6112228a40e497f99662
Malbathan Lima <malbathanlima@gmail.com>
2021-02-24 16:11:31 -0300

    initial commit

---
+++ Bloco_4.1/dia_1/scripts.js
@@ -0,0 +1,9 @@
+const name = "Malbathan";
+const birthCity = "Ipiranga - SP";
+// birthCity = "Sao Paulo"
+let birthYear = 1991;
+birthYear = 2020;
+
+console.log(birthCity);
+console.log(name);
+console.log(birthYear);
