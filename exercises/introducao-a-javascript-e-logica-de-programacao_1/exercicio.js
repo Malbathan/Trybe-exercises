41697da243dd2aec777df9dbb98514769990fc82
Malbathan Lima <malbathanlima@gmail.com>
2021-02-24 18:27:22 -0300

    programa para comparaçao de maior numero entre variveis

--- exercises/introducao-a-javascript-e-logica-de-programacao_1/exercicio.js
+++ exercises/introducao-a-javascript-e-logica-de-programacao_1/exercicio.js
@@ -1,8 +1,14 @@
 let a = 2;
 let b = 5;
 
-console.log (a + b);
-console.log (a - b);
-console.log (a * b);
-console.log (a / b);
-console.log (a % b);
+// console.log (a + b);
+// console.log (a - b);
+// console.log (a * b);
+// console.log (a / b);
+// console.log (a % b);
+
+if (a > b) {
+    console.log(a);
+} else {
+    console.log(b);
+}
