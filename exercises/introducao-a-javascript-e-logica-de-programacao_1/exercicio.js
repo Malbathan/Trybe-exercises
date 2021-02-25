3c362936df25430fb8f1b69078365935ab505110
Malbathan Lima <malbathanlima@gmail.com>
2021-02-25 12:18:37 -0300

    exercicio 4 - variaveis positivas negativas e zero

--- exercises/introducao-a-javascript-e-logica-de-programacao_1/exercicio.js
+++ exercises/introducao-a-javascript-e-logica-de-programacao_1/exercicio.js
@@ -13,12 +13,33 @@ let b = 5;
 //     console.log(b);
 // }
 
-let c = 7;
+// let c = 7;
 
-if (a > b && a > c) {
-    console.log (a);
-} else if (b > a && b > c) { 
-    console.log (b);
-} else {
-    console.log (c);
+// if (a > b && a > c) {
+//     console.log (a);
+// } else if (b > a && b > c) { 
+//     console.log (b);
+// } else {
+//     console.log (c);
+// }
+
+// * Problema resolvido graças ao codigo de exemplo da Fernanda Porto - Turma 10 - Tribo B-
+
+let number = 0;
+
+switch (true) {
+    case (number>0):
+    console.log ("positive");
+    break;
+
+    case (number<0):
+    console.log ("negative");
+    break;
+
+    case (number==0):
+    console.log ("zero");
+    break;
+
+    default:
+    console.log ("Erro");
 }
