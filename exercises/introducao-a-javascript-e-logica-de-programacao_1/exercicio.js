bdde0c2aa811544748be85c92fe160ea7ace11f5
Malbathan Lima <malbathanlima@gmail.com>
2021-02-25 12:46:22 -0300

    Exercicio de peças de xadrez

--- exercises/introducao-a-javascript-e-logica-de-programacao_1/exercicio.js
+++ exercises/introducao-a-javascript-e-logica-de-programacao_1/exercicio.js
@@ -44,12 +44,37 @@
 //     console.log ("Erro");
 // }
 
-let anguloA = 90;
-let anguloB = 45;
-let anguloC = 45;
+// let anguloA = 90;
+// let anguloB = 45;
+// let anguloC = 45;
 
-if (anguloA + anguloB + anguloC == 180) {
-    console.log( "true")
-} else {
-    console.log ("false")
+// if (anguloA + anguloB + anguloC == 180) {
+//     console.log( "true")
+// } else {
+//     console.log ("false")
+// }
+
+let pieceXadrez = "rei";
+
+switch (pieceXadrez.toLowerCase()){
+    case "peão" :
+        console.log ("Ande uma casa para frente e capture apenas nas diagonais.");
+        break;
+    case "torre" :
+        console.log ("Ande e/ou capture para frente ou para o lado, quantas casas quiser.");
+        break;
+    case "bispo" :
+        console.log ("Ande e/ou capture em diagonal, quantas casas quiser.");
+        break;
+    case "cavalo" :
+        console.log ("Ande e capture em formato de L, apenas quatro casas.");
+        break;
+    case "rainha" :
+        console.log ("Ande e capture em todas as direções, quantas casas quiser.");
+        break;
+    case "rei" :
+        console.log ("Ande e capture em todas as direções, apenas uma casa.");
+        break;
+    default : 
+        console.log ("Digite uma peça existente.");
 }
