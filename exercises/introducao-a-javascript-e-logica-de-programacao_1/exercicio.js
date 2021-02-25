82100e1dde009695cc5c98c81d3927019e503509
Malbathan Lima <malbathanlima@gmail.com>
2021-02-25 13:12:27 -0300

    Exercicio de nota em porcentagem

--- exercises/introducao-a-javascript-e-logica-de-programacao_1/exercicio.js
+++ exercises/introducao-a-javascript-e-logica-de-programacao_1/exercicio.js
@@ -54,27 +54,54 @@
 //     console.log ("false")
 // }
 
-let pieceXadrez = "rei";
+// let pieceXadrez = "rei";
 
-switch (pieceXadrez.toLowerCase()){
-    case "peão" :
-        console.log ("Ande uma casa para frente e capture apenas nas diagonais.");
+// switch (pieceXadrez.toLowerCase()){
+//     case "peão" :
+//         console.log ("Ande uma casa para frente e capture apenas nas diagonais.");
+//         break;
+//     case "torre" :
+//         console.log ("Ande e/ou capture para frente ou para o lado, quantas casas quiser.");
+//         break;
+//     case "bispo" :
+//         console.log ("Ande e/ou capture em diagonal, quantas casas quiser.");
+//         break;
+//     case "cavalo" :
+//         console.log ("Ande e capture em formato de L, apenas quatro casas.");
+//         break;
+//     case "rainha" :
+//         console.log ("Ande e capture em todas as direções, quantas casas quiser.");
+//         break;
+//     case "rei" :
+//         console.log ("Ande e capture em todas as direções, apenas uma casa.");
+//         break;
+//     default : 
+//         console.log ("Digite uma peça existente.");
+// }
+
+// * desenvolvido a partir do exemplo da Fernanda Porto - Turma 10 - tribo B
+let nota = 10;
+// * Aqui eu acredito que se a nota for usada de 0 a 10, vai ser necessario uma expressão para calcular isso em porcentagem.
+
+switch (true) {
+    case (nota >= 90 && nota <= 100):
+        console.log ("nota : A");
         break;
-    case "torre" :
-        console.log ("Ande e/ou capture para frente ou para o lado, quantas casas quiser.");
+    case (nota >= 80 && nota <90):
+        console.log ("nota : B");
         break;
-    case "bispo" :
-        console.log ("Ande e/ou capture em diagonal, quantas casas quiser.");
+    case (nota >= 70 && nota <80):
+        console.log ("nota : C");
         break;
-    case "cavalo" :
-        console.log ("Ande e capture em formato de L, apenas quatro casas.");
+    case (nota >= 60 && nota <70):
+        console.log ("nota : D");
         break;
-    case "rainha" :
-        console.log ("Ande e capture em todas as direções, quantas casas quiser.");
+    case (nota >= 50 && nota <60):
+        console.log ("nota : E");
         break;
-    case "rei" :
-        console.log ("Ande e capture em todas as direções, apenas uma casa.");
+    case (nota <50):
+        console.log ("nota : F");
         break;
-    default : 
-        console.log ("Digite uma peça existente.");
+    default:
+        console.log ("Nota inválida");
 }
