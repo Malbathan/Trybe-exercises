58cea80f66d752630c91f273eb657de20e15a0c0
Malbathan Lima <malbathanlima@gmail.com>
2021-02-26 19:30:28 -0300

    exercicio 1

---
+++ exercises/introducao-a-javascript-e-logica-de-programacao_3/exercise.js
@@ -0,0 +1,18 @@
+// 1- Para o primeiro exercício de hoje, faça um programa que, dado um valor n qualquer, seja n > 1 , imprima na tela um quadrado feito de asteriscos de lado de tamanho n . Por exemplo:
+
+let asterisk = 5;
+let out = "";
+for (let indexA = 0; indexA < asterisk ; indexA += 1){
+    for (let indexOut = 0; indexOut < asterisk; indexOut += 1) {
+    out += "*";
+    }
+    console.log(out);
+    out = "";
+}
+
+// 2- Para o segundo exercício, faça o mesmo que antes, mas que imprima um triângulo retângulo com 5 asteriscos de base. Por exemplo:
+
+// 3- Agora inverta o lado do triângulo. Por exemplo:
+
+// 4- Depois, faça uma pirâmide com n asteriscos de base:
+
