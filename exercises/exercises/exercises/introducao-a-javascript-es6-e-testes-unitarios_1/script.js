0c2907f1b6bcfac3566c136682bef2ffd3401eb0
Malbathan Lima <malbathanlima@gmail.com>
2021-03-23 18:31:20 -0300

    exercicio 02

--- exercises/exercises/exercises/introducao-a-javascript-es6-e-testes-unitarios_1/script.js
+++ exercises/exercises/exercises/introducao-a-javascript-es6-e-testes-unitarios_1/script.js
@@ -1,12 +1,19 @@
 const testingScope = escopo => {
     if (escopo === true) {
-      var ifScope = 'Não devo ser utilizada fora do meu escopo (if)';
+      let ifScope = 'Não devo ser utilizada fora do meu escopo (if)';
       ifScope = `${ifScope} ótimo, fui utilizada no escopo !`;
       console.log(ifScope);
     } else {
-      var elseScope = 'Não devo ser utilizada fora meu escopo (else)';
+      const elseScope = 'Não devo ser utilizada fora meu escopo (else)';
       console.log(elseScope);
     }
   }
 
   testingScope(true);
+
+
+  const oddsAndEvens = [13, 3, 4, 10, 7, 2];
+  oddsAndEvens.sort(function(a, b) {
+  return a - b;
+  });
+  console.log(`Os números ${oddsAndEvens} se encontram ordenados de forma crescente !`);
