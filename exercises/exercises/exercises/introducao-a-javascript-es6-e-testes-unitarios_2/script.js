4a2594a98ddddfaca1e5985b154f7d92896f395e
Malbathan Lima <malbathanlima@gmail.com>
2021-03-29 18:01:59 -0300

    inittialing

--- exercises/exercises/exercises/introducao-a-javascript-es6-e-testes-unitarios_2/script.js
+++ exercises/exercises/exercises/introducao-a-javascript-es6-e-testes-unitarios_2/script.js
@@ -1,74 +1,64 @@
 const order = {
-    name: 'Rafael Andrade',
-    phoneNumber: '11-98763-1416',
-    address: {
-      street: 'Rua das Flores',
-      number: '389',
-      apartment: '701',
+  name: 'Rafael Andrade',
+  phoneNumber: '11-98763-1416',
+  address: {
+    street: 'Rua das Flores',
+    number: '389',
+    apartment: '701',
+  },
+  order: {
+    pizza: {
+    margherita: {
+      amount: 1,
+      price: 25,
     },
-    order: {
-      pizza: {
-        margherita: {
-          amount: 1,
-          price: 25,
-        },
-        pepperoni: {
-          amount: 1,
-          price: 20,
-        }
-      },
-      drinks: {
-        coke: {
-          type: 'Coca-Cola Zero',
-          price: 10,
-          amount: 1,
-        }
-      },
-      delivery: {
-        deliveryPerson: 'Ana Silveira',
-        price: 5,
-      }
+    pepperoni: {
+      amount: 1,
+      price: 20,
+    }
     },
-    payment: {
-      total: 60,
+    drinks: {
+    coke: {
+      type: 'Coca-Cola Zero',
+      price: 10,
+      amount: 1,
+    }
     },
+    delivery: {
+    deliveryPerson: 'Ana Silveira',
+    price: 5,
+    }
+  },
+  payment: {
+    total: 60,
+  },
   };
  
 //Complete a função customerInfo() para que seu retorno seja similar a "Olá Ana Silveira, entrega para: Rafael Andrade, Telefone: 11-98763-1416, R. Rua das Flores, Nº: 389, AP: 701".
-const customerInfo = (order) => {
-  // Adicione abaixo as informações necessárias.
-  const deliveryPerson = order.order.delivery.deliveryPerson;
-  const name = order.name;
-  const phone = order.phoneNumber;
-  const address = order.address;
-  const street = order.address.street;
-  const number = order.address.number;
-  const apartment = order.address.apartment;
+// const customerInfo = (order) => {
+//   // Adicione abaixo as informações necessárias.
+//   const deliveryPerson = order.order.delivery.deliveryPerson;
+//   const name = order.name;
+//   const phone = order.phoneNumber;
+//   const address = order.address;
+//   const street = order.address.street;
+//   const number = order.address.number;
+//   const apartment = order.address.apartment;
 
-    console.log(`Olá ${deliveryPerson}, entrega para: ${name}, Telefone: ${phone}, R. ${street}, Nº: ${number}, AP: ${apartment}`);
-}
+//   console.log(`Olá ${deliveryPerson}, entrega para: ${name}, Telefone: ${phone}, R. ${street}, Nº: ${number}, AP: ${apartment}`);
+// }
 
-customerInfo(order);
+// customerInfo(order);
 
+//Complete a função orderModifier() para que seu retorno seja similar a "Olá Luiz Silva, o total do seu pedido de muzzarella, calabresa e Coca-Cola Zero é R$ 50,00."
 const orderModifier = (order) => {
   // Adicione abaixo as informações necessárias.
+  const name = order.name = 'Luiz Silva';
+  const pizzas = order.order.pizza = 'muzzarela, calabresa';
+  const coke = order.order.drinks.coke.type;
+  const payment = order.payment.total = 50
 
-}
-
-orderModifier(order);
-  },
-};
-
-const customerInfo = (order) => {
-  // Adicione abaixo as informações necessárias.
-
-}
-
-customerInfo(order);
-
-const orderModifier = (order) => {
-  // Adicione abaixo as informações necessárias.
-
+  console.log(`Olá ${name}, o total do seu pedido de ${pizzas} e ${coke} é R$ ${payment},00.`);
 }
 
 orderModifier(order);
